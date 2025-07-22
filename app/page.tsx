977a986dd3d21e33128dc6ecd29e392ac0a7381e
alibazregarijs <alibazregariintel@gmail.com>
2025-07-22 07:47:06 +0330

    fix tailwind.config.ts

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function ButtonDemo() {
   return (
     <div className="flex flex-wrap items-center gap-2 md:flex-row">
       <Button>Button</Button>
-      <h1 className="bg-red-500"> hello </h1>
+      <h1 className="bg-orange-1 text-xl">Hello world</h1>
     </div>
   )
 }
