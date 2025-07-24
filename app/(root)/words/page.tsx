f8e2469b5455c037604a29b354c7b36dc41235f9
alibazregarijs <alibazregariintel@gmail.com>
2025-07-24 12:19:57 +0330

    callback function

--- app/(root)/words/page.tsx
+++ app/(root)/words/page.tsx
@@ -1,7 +1,51 @@
+"use client";
+
 import React from "react";
+import { CarouselDemo } from "@/components/Carousel";
+
+export type wordProps = {
+  audioDefinitionStorageId: string;
+  user: string;
+  definition: string;
+  word: string;
+  isSeen: boolean;
+  audioWordUrl: string;
+};
 
 const Word = () => {
-  return <div>Word</div>;
+  const [slideIndex, setSlideIndex] = React.useState(0);
+  const [words, setWords] = React.useState<wordProps[]>([
+    {
+      audioDefinitionStorageId: "1",
+      user: "1",
+      definition: "Definition 1",
+      word: "Word 1",
+      isSeen: false,
+      audioWordUrl: "https://www.example.com/audio.mp3",
+    },
+    {
+      audioDefinitionStorageId: "2",
+      user: "2",
+      definition: "Definition 2",
+      word: "Word 2",
+      isSeen: false,
+      audioWordUrl: "https://www.example.com/audio.mp3",
+    },
+  ]);
+
+  const handleNextButton = () => {
+    setSlideIndex((prevIndex) => prevIndex + 1);
+  };
+
+  const handlePreviousButton = () => {
+    setSlideIndex((prevIndex) => prevIndex - 1);
+  };
+
+  return (
+    <div className="flex-center h-full">
+      <CarouselDemo onNextClick={handleNextButton} onPrevClick={handlePreviousButton} word={words[slideIndex]} />
+    </div>
+  );
 };
 
 export default Word;
---
+++ components/Carousel.tsx
@@ -0,0 +1,47 @@
+import * as React from "react";
+
+import { Card, CardContent } from "@/components/ui/card";
+import { wordProps } from "@/app/(root)/words/page";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
+
+export function CarouselDemo({
+  onNextClick,
+  onPrevClick,
+  word,
+}: {
+  onNextClick: () => void;
+  onPrevClick: () => void;
+  word: wordProps;
+}) {
+  console.log(word);
+  return (
+    <Carousel className="w-full max-w-xs">
+      <CarouselContent>
+        {Array.from({ length: 5 }).map((_, index) => (
+          <CarouselItem key={index}>
+            <div className="p-1">
+              <Card>
+                <CardContent className="flex aspect-square items-center justify-center p-6">
+                  <span className="text-4xl font-semibold">{index + 1}</span>
+                </CardContent>
+              </Card>
+            </div>
+          </CarouselItem>
+        ))}
+      </CarouselContent>
+      <div onClick={onPrevClick}>
+        <CarouselPrevious />
+      </div>
+
+      <div onClick={onNextClick}>
+        <CarouselNext />
+      </div>
+    </Carousel>
+  );
+}
