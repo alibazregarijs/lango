e7be9cf1f3eb3b426ee24339eb25839a5bc280f2
alibazregarijs <alibazregariintel@gmail.com>
2025-07-26 06:19:16 +0330

    words

--- app/(root)/words/page.tsx
+++ app/(root)/words/page.tsx
@@ -1,72 +1,17 @@
 "use client";
 
-import essentialWords from "@/words.json";
 import React, { useEffect, useRef, useState } from "react";
-import axios from "axios";
 import { CarouselDemo } from "@/components/Carousel";
-
-export type WordObject = {
-  definition: {
-    definition: string;
-    example: string;
-  }[];
-  word: string;
-  type: {
-    partOfSpeech: string;
-  }[];
-  audioWordUrl: {
-    audio: string;
-  }[];
-  meaningCount: number;
-};
+import { type WordObject } from "@/types";
+import { fetchRandomWord } from "@/api";
 
 const Word = () => {
   const slideIndexRef = useRef(0);
   const [, forceUpdate] = useState(0);
   const [words, setWords] = useState<WordObject[]>([]);
 
-  const fetchRandomWord = async () => {
-    try {
-      const randomWord =
-        essentialWords[Math.floor(Math.random() * essentialWords.length)];
-
-      const apiWord = randomWord;
-      const randomWordObj = await axios.get(
-        `https://api.dictionaryapi.dev/api/v2/entries/en/${apiWord}`
-      );
-
-      const apiData = randomWordObj.data[0];
-
-      const newWord: WordObject = {
-        word: apiWord,
-        meaningCount: apiData.meanings.length || 0,
-        definition: [],
-        type: [],
-        audioWordUrl: [],
-      };
-
-      apiData.meanings.forEach((item: any) => {
-        newWord.type.push({ partOfSpeech: item.partOfSpeech });
-        newWord.definition.push({
-          definition: item.definitions[0]?.definition || "",
-          example: item.definitions[0]?.example || "",
-        });
-      });
-
-      apiData.phonetics.forEach((item: any) => {
-        newWord.audioWordUrl.push({
-          audio: item.audio || "",
-        });
-      });
-
-      setWords((prev) => [...prev, newWord]);
-    } catch (error) {
-      console.error("Failed to fetch random word:", error);
-    }
-  };
-
   useEffect(() => {
-    fetchRandomWord(); // Fetch initial word
+    fetchRandomWord({ setWords }); // Fetch initial word
   }, []);
 
   const handleNextButton = () => {
@@ -74,7 +19,7 @@ const Word = () => {
 
     // When reaching end, fetch new word
     if (slideIndexRef.current === words.length) {
-      fetchRandomWord();
+      fetchRandomWord({ setWords });
     } else {
       forceUpdate((prev) => prev + 1);
     }
--- components/Carousel.tsx
+++ components/Carousel.tsx
@@ -1,5 +1,4 @@
 import * as React from "react";
-import { Card } from "@/components/ui/card";
 import {
   Carousel,
   CarouselContent,
@@ -7,7 +6,7 @@ import {
   CarouselNext,
   CarouselPrevious,
 } from "@/components/ui/carousel";
-import { useState } from "react";
+import { WordObject } from "@/types";
 
 export function CarouselDemo({
   onNextClick,
@@ -17,7 +16,7 @@ export function CarouselDemo({
 }: {
   onNextClick: () => void;
   onPrevClick: () => void;
-  words: any[];
+  words: WordObject[];
   slideIndex: number;
 }) {
   const speak = () => {
@@ -33,45 +32,50 @@ export function CarouselDemo({
             Random<span className="text-orange-1 ml-2">Word</span>
           </span>
         </div>
-        {words[slideIndex]?.type?.map((item: any, index: number) => (
-          <div key={index}>
-            {item.partOfSpeech.length > 1 && (
-              <div key={index}>
-                <CarouselItem>
-                  <div className="flex flex-col h-[70vh] overflow-y-scroll no-scrollbar ml-2">
-                    <div className="flex flex-col">
-                      <div className="flex flex-col ml-4 space-y-1 flex-grow">
-                        <span className="text-medium font-semibold capitalize">
-                          {words[slideIndex]?.word}
-                        </span>
-                        <div className="flex flex-col">
-                          <span className="text-[12px] text-gray-400">
-                            As a {item.partOfSpeech}
+        {words[slideIndex]?.type?.map(
+          (item: WordObject["type"][0], index: number) => (
+            <div key={index}>
+              {item.partOfSpeech.length > 1 && (
+                <div key={index}>
+                  <CarouselItem>
+                    <div className="flex flex-col h-[70vh] overflow-y-scroll no-scrollbar ml-2">
+                      <div className="flex flex-col">
+                        <div className="flex flex-col ml-4 space-y-1 flex-grow">
+                          <span className="text-medium font-semibold capitalize">
+                            {words[slideIndex]?.word}
                           </span>
+                          <div className="flex flex-col">
+                            <span className="text-[12px] text-gray-400">
+                              As a {item.partOfSpeech}
+                            </span>
 
-                          <div className="mt-2 space-y-2 mb-20">
-                            <p>
-                              {words[slideIndex]?.definition[index]?.definition}
-                            </p>
+                            <div className="mt-2 space-y-2 mb-20">
+                              <p>
+                                {
+                                  words[slideIndex]?.definition[index]
+                                    ?.definition
+                                }
+                              </p>
 
-                            <p className="text-gray-400 text-[12px]">
-                              {words[slideIndex]?.definition[index]?.example}
-                            </p>
+                              <p className="text-gray-400 text-[12px]">
+                                {words[slideIndex]?.definition[index]?.example}
+                              </p>
+                            </div>
+                          </div>
+                        </div>
+                        <div className="flex-center">
+                          <div>
+                            <button onClick={speak}>Speak</button>
                           </div>
                         </div>
                       </div>
-                      <div className="flex-center">
-                        <div>
-                          <button onClick={speak}>Speak</button>
-                        </div>
-                      </div>
                     </div>
-                  </div>
-                </CarouselItem>
-              </div>
-            )}
-          </div>
-        ))}
+                  </CarouselItem>
+                </div>
+              )}
+            </div>
+          )
+        )}
       </CarouselContent>
       <div onClick={onPrevClick}>
         <CarouselPrevious className="text-white hover:text-gray-200" />
